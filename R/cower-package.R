569b7bceece0b92608966395ec4d674106329633
m-Py <martin.papenberg@hhu.de>
2015-08-20 14:51:37 +0200

    rebuild package

---
+++ R/cower-package.R
@@ -0,0 +1,22 @@
+#' Power Analysis for Correlation Coefficients.
+#'
+#' \tabular{ll}{
+#' Package: \tab cower\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.01-1\cr
+#' Date: \tab 2015-08-20\cr
+#' Depends: \tab R (>= 2.10.0)\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Conduct power analyses for correlation coefficients
+#'
+#' @aliases cower-package
+#' @name cower-package
+#' @docType package
+#' @title The cower Package
+#' @author Martin Papenberg
+#' @keywords package
+NULL
