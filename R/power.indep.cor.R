dc4f059a595d3f183592c347b708b9d561f4f212
m-Py <martin.papenberg@hhu.de>
2015-08-20 13:09:51 +0200

    added a priori analysis

--- R/power.indep.cor.R
+++ R/power.indep.cor.R
@@ -21,34 +21,96 @@
 #' @export
 
 
-power.indep.cor <- function(r1, r2, n1, n2, sig.level = 0.05, alternative = "two.sided") {
+power.indep.cor <- function(r1, r2, n1 = NULL, n2 = NULL, power = NULL, sig.level = 0.05, alternative = "two.sided") {
    
    # add some error handling
    if (alternative != "two.sided" && alternative != "less" && alternative != "greater") {
       stop("alternative must be 'two.sided', 'less', or 'greater'.")
    }
+   if ( (is.null(n1) && is.null(power)) || (is.null(n2) && is.null(power)) ) {
+      stop("either n1 and n2 or power must be given, not both")
+   }
+   if ( is.null(n1) && is.null(n2) && is.null(power) ) {
+      stop("either n1 and n2 or power must be given, not both")
+   }
 
    effSizeQ         <- atanh(r1) - atanh(r2)           # difference of two Fischer-z-transformed r -> effect size q
-   s                <- sqrt( 1/(n1 - 3) + 1/(n2 - 3) ) # see G-Power 3.1 manual
-   distributionH1   <- effSizeQ/s                      # mean of the effect size distribution assuming an effect
    
-   if (alternative  == "less") {
-      criticalValue  <- qnorm(sig.level)
-      pwr            <- 1 - pnorm(criticalValue, mean = distributionH1, sd = 1)
+   # prepare which type of analysis to do: determine power or n?
+   if (!is.null(power)) {
+      wantedPower      <- power
+      startPoint <- 3
+      i <- 0 # for approximating power
+      n1 <- startPoint + 2^i
+      n2 <- startPoint + 2^i
+      status <- "increase" # start increasing n in power approximation
+      steps  <- 0 # how exact should approximation be
    }
-   else if (alternative == "greater") {
-      criticalValue  <- qnorm(1-sig.level)
-      pwr            <- 1 - pnorm(criticalValue, mean = distributionH1, sd = 1)
+   else { 
+      wantedPower <- Inf # not relevant if power is to be determined
    }
-   else if (alternative == "two.sided") {
-      if (r1 > r2) {
-         criticalValue <- qnorm(1-(sig.level/2))
-         pwr           <- 1 - pnorm(criticalValue, mean = distributionH1, sd = 1)
+   
+   currentPower     <- 0                               # approximate power   
+   bestFitFound     <- FALSE
+   
+   # loop until power is reached (if n is searched for)
+   while (bestFitFound == FALSE) {
+      s                <- sqrt( 1/(n1 - 3) + 1/(n2 - 3) ) # see G-Power 3.1 manual
+      distributionH1   <- effSizeQ/s                      # mean of the effect size distribution assuming an effect
+      
+      #### determine power for given N
+      if (alternative  == "less") {
+         criticalValue  <- qnorm(sig.level)
+         pwr            <- 1 - pnorm(criticalValue, mean = distributionH1, sd = 1)
       }
-      else if (r1 < r2) {
-         criticalValue <- qnorm(sig.level/2)
-         pwr           <- pnorm(criticalValue, mean = distributionH1, sd = 1)
+      else if (alternative == "greater") {
+         criticalValue  <- qnorm(1-sig.level)
+         pwr            <- 1 - pnorm(criticalValue, mean = distributionH1, sd = 1)
+      }
+      else if (alternative == "two.sided") {
+         if (r1 > r2) {
+            criticalValue <- qnorm(1-(sig.level/2))
+            pwr           <- 1 - pnorm(criticalValue, mean = distributionH1, sd = 1)
+         }
+         else if (r1 < r2) {
+            criticalValue <- qnorm(sig.level/2)
+            pwr           <- pnorm(criticalValue, mean = distributionH1, sd = 1)
+         }
+      }
+      if (is.null(power)) { 
+         break # need not loop if power is to be determined
+      } 
+      
+      ### CASE: determine n, continue looping until good fit for power is found
+      currentPower <- pwr
+      i  <- i+1
+      if (steps == 10 && currentPower >= power) { # make sure that approximation is rather exact
+         bestFitFound <- TRUE
+         print(paste("n per condition", n1))
+         return(pwr)
+      }
+      if (currentPower < wantedPower) { # increase n
+         if (status == "decrease") {
+            i          <- 0
+            startPoint <- n1
+            steps      <- steps + 1
+         }
+         status <- "increase"
+         n1 <- startPoint + 2^i
+         n2 <- startPoint + 2^i
+      }
+      else if (currentPower >= wantedPower) {
+         if (status == "increase") {
+            i          <- 0
+            startPoint <- n1
+            steps      <- steps + 1
+         }
+         status <- "decrease"
+         n1         <- startPoint - 2^i
+         n2         <- startPoint - 2^i
       }
    }
+   
+   print(c(n1, n2))
    return(pwr)
 }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pwr_cor
 
-Package to conduct power analyses for the comparison of correlation coefficients. Currently, one function is available, which can be used to determine the power for detecting the difference between two correlation coefficients. Results are tested against <a href ="http://www.gpower.hhu.de/">G-Power 3.1</a>.
+Package to conduct power analyses for the comparison of correlation coefficients. Currently, one function is available, which can be used conduct power analyses for detecting the difference between two independent correlation coefficients. Results are tested against <a href ="http://www.gpower.hhu.de/" target="_blank">G-Power 3.1</a>.
+
+Types of power analysis are 
+
+- "post-hoc" - determine the power for two given correlation coefficients and two given n
+- "a priori" - specify a desired power and two correlation coefficients, to determine the required n
+
